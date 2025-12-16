26f5428e432f2bdbc37c042384425f050fae0167
Beris04 <gueroberistain@gmail.com>
2025-12-16 09:47:09 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,8 @@
-const CACHE_NAME = "toyo-visitas-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open("toyo-v1").then(c=>c.addAll(["./","./index.html"]))
+ );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
