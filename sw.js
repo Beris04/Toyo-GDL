dec08432eab2ec671e2ae127804c17072e17f52a
Beris04 <gueroberistain@gmail.com>
2026-08-21 16:12:36 -0600

    3.1v

--- sw.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>{e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>/^toyo-comercial-/i.test(k)).map(k=>caches.delete(k)));await self.registration.unregister();await self.clients.claim();})())});
